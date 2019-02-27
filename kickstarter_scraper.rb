cc9ced56c8a1c8e9301bcbcbf67f50533f3767c2
Kelsey Low <kklowlow@gmail.com>
2019-02-27 02:11:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,4 +18,4 @@ create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("span.location").text
+# location: project.css("span.location-name").text
