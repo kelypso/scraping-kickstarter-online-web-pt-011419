4ad649450b3d33f553f163ba52fedc596e1a790c
Kelsey Low <kklowlow@gmail.com>
2019-02-27 02:29:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,12 @@ def create_project_hash
   # iterate through projects 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
   end
  
   projects
@@ -24,7 +29,7 @@ create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
-# image: project.css("div.project-thumbnail a img").attribute("src").value
+# image: 
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
